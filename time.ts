f2c1aa9350fffc1e82b97d19f2f48bcb192f47c6
an.je@altsoft.kr <155497479+jeongeum1202@users.noreply.github.com>
2024-01-23 19:43:17 +0900

    [COMMON] Profile 컴포넌트 (#60)
    * :bento: Chore: 기본 프로필 정적이미지 추가 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: Avatar 컴포넌트 구현 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: content base profile 컴포넌트 구현 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: comment profile 컴포넌트 구현 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: localbox profile 컴포넌트 구현 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: my profile 컴포넌트 구현 (#56) (ONDAJI4-377)
    
    * :pencil2: Chore: index.ts 추가 및 불필요한 코드 삭제 (#56) (ONDAJI4-377)
    
    * :lipstick: Design: 아바타 컴포넌트 크기 추가 (#56) (ONDAJI4-377)
    
    * :bento: Chore: 좋아요 하트 아이콘 추가 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: time props 추가 및 comment 프로필 컴포넌트 스토리 추가 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: 프로필 컴포넌트 withState 스토리 추가 (#56) (ONDAJI4-377)
    
    * :lipstick: Design: 아이콘과 텍스트 사이 간격을 상수로 변경 (#56) (ONDAJI4-377)
    
    * :speech_balloon: Chore: 크리에이터 타입에 대해 리터럴 타입으로 변경 (#56) (ONDAJI4-377)
    
    * :lipstick: Design: 아바타 컴포넌트 size 단위형태로 변경 (#56) (ONDAJI4-377)
    
    * :sparkles: Feat: 시간 계산 함수를 util에 추가 (#56) (ONDAJI4-377)
    
    * :lipstick: Design: 아바타 컴포넌트 크기 오류 발생하는 부분 수정 (#56) (ONDAJI4-377)
    
    * :lipstick: Design: 디자인 시스템 수정으로 프로필 컴포넌트 typo 수정 (#56) (ONDAJI4-377)

---
+++ time.ts
@@ -0,0 +1,25 @@
+/* 시간 계산 함수 */
+export const timeFuc = (commentTime: string) => {
+  const today = new Date();
+  const timeValue = new Date(commentTime);
+
+  const betweenTime = Math.floor(
+    (today.getTime() - timeValue.getTime()) / 1000 / 60
+  );
+  if (betweenTime < 1) return '방금 전';
+  if (betweenTime < 60) {
+    return `${betweenTime}분 전`;
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60);
+  if (betweenTimeHour < 24) {
+    return `${betweenTimeHour}시간 전`;
+  }
+
+  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+  if (betweenTimeDay < 365) {
+    return `${betweenTimeDay}일 전`;
+  }
+
+  return `${Math.floor(betweenTimeDay / 365)}년 전`;
+};
