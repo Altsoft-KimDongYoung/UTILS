9059d0a01ed048c990aa914b81c7f33c28ce2dbf
kim.dy@altsoft.kr <155496890+Altsoft-KimDongYoung@users.noreply.github.com>
2024-02-23 10:04:27 +0900

    [COMMON] Dashboard 컴포넌트 구현 (#92)
    * ✨ Feat: dashboard 컴포넌트
    
    * ✨ Feat: formatNumberWithSuffix 유틸하무
    
    * 🔧Chore: yarn build 테스트

--- number.ts
+++ number.ts
@@ -4,3 +4,31 @@ export const numberWithCommas = (number: number) => {
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return parts.join('.');
 };
+
+/* 숫자를 K, M 과 같은 suffix로 축약하는 함수 */
+export const formatNumberWithSuffix = (number: number) => {
+  const million = 1000000;
+  const thousand = 1000;
+
+  const format = (number: number, suffix: string) => {
+    const parts = number.toString().split('.');
+
+    const firstDecimal = parts[1]?.[0] || null;
+    const quotient = parts[0];
+    const hasDecimal = parts.length > 1;
+    const isFirstDecimalZero = firstDecimal === '0';
+    const isQuotientThreeDigits = quotient.length === 3;
+
+    const decimal =
+      hasDecimal && !isFirstDecimalZero && !isQuotientThreeDigits
+        ? '.' + firstDecimal
+        : '';
+    return quotient + decimal + suffix;
+  };
+
+  if (number >= million) return format(number / million, 'm');
+
+  if (number >= thousand) return format(number / thousand, 'k');
+
+  return number.toString();
+};
