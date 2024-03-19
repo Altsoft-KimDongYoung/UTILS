09320842e8299562048e8ca6459cd86580fba57b
an.je@altsoft.kr <155497479+jeongeum1202@users.noreply.github.com>
2024-03-19 16:20:43 +0900

    [COMMON] Modal 컴포넌트 구현 (#107)
    * :bento: Chore: 모달에 사용되는 이미지 추가
    
    * :sparkles: Feat: Portal 컴포넌트 구현
    
    * :sparkles: Feat: 조사 유틸 함수 구현
    
    * :sparkles: Feat: Modal 컴포넌트 및 스토리북 구현
    
    * :sparkles: Feat: modal store 구현
    
    * :sparkles: Feat: 종류별 Modal 컴포넌트 구현
    
    - TextModal, TitleModal, ImageModal, NameModal 구현
    - 공통된 스타일이 많아 하나로 합치는 수정 필요할 것 같음
    
    * :lipstick: Design: lint-css 적용
    
    * :recycle: Refactor: image variant 로 변경
    
    * :truck: Rename: store 폴더 명 stores로 수정
    
    * :recycle: Refactor: type 컨벤션에 맞춰 interface로 변경
    
    * :recycle: Refactor: imageType keyof 사용으로 코드 수정
    
    * :speech_balloon: Chore: 불필요한 코드 삭제
    
    * :speech_balloon: Chore: import-order 적용으로 인한 코드 수정
    
    * Merge branch 'develop' into feature/COMM_2101
    
    * :lipstick: Design: 충돌로 인한 코드 수정
    
    * :recycle: Refactor: 코드리뷰 피드백 반영
    
    1. Modal 컴포넌트
    - isModalOpen 연산자 &&로 변경
    - props 삭제
    - Modal 컴포넌트 최상위에 Portal 추가
    - type을 interface로 변경
    
    2. 각 타입별 모달 컴포넌트
    - Group을 Container로 네이밍 변경
    - Image 태그에 sizes 추가
    - Image 태그 내 alt값 수정
    - name 분기처리 삭제
    - 타입 첫 글자 네이밍 대문자로 변경
    
    3. modal store
    - immer 사용방식 변경
    
    * :recycle: Refactor: 코드리뷰 피드백 반영
    
    - 모달 description 스타일에서 사용되지 않는 코드 삭제

---
+++ josa.ts
@@ -0,0 +1,30 @@
+export type JosaOption = '이' | '가' | '이/가' | '을' | '를' | '을/를';
+
+const checkKorean = (word: string) => {
+  const lastChar = word.charCodeAt(word.length - 1);
+  const isThereLastChar = (lastChar - 0xac00) % 28;
+
+  return Boolean(isThereLastChar);
+};
+
+const formats = (word: string, josa: JosaOption) => {
+  switch (josa) {
+    case '이':
+    case '가':
+    case '이/가':
+      return checkKorean(word) ? '이' : '가';
+    case '을':
+    case '를':
+    case '을/를':
+      return checkKorean(word) ? '을' : '를';
+    default:
+      throw 'Invalid format!';
+  }
+};
+
+export const addJosa = (word: string, josa: JosaOption) => {
+  if (typeof josa === 'undefined') throw 'invalid format!';
+  const formattedJosa = formats(word, josa);
+
+  return formattedJosa;
+};
