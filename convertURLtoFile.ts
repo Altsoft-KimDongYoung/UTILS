ba99413c77f8a610f076e147b6509b32089ae386
an.je@altsoft.kr <155497479+jeongeum1202@users.noreply.github.com>
2024-04-23 16:27:08 +0900

    [CONT] 동네소식 등록 - 임시저장 페이지 구현 (#125)
    * :bento: Chore: 바텀시트에 필요한 이미지 추가
    
    * :recycle: Refactor: 바텀시트 디자인 변경으로 인한 리팩토링
    
    * :sparkles: Feat: 유형 별 콘텐츠 등록 바텀시트 분기 처리 기능 추가
    
    * :sparkles: Feat: 바텀시트 header case 추가
    
    - 임시저장 바텀시트 추가로 인해 왼쪽 close, 오른쪽 badgeButton이 있는 header 추가
    
    * :recycle: Refactor: 임시저장 체크박스 디자인 추가로 인한 코드 수정
    
    * :lipstick: Design: lint-css 적용
    
    * :sparkles: Feat: Frame 컴포넌트 bottomButton에 disabled 속성 추가
    
    * :truck: Rename: 사이니지 에디터 가이드 폴더 이동으로 인한 코드 수정
    
    * :bug: Fix: hydrate mismatch 에러 해결
    
    * :sparkles: Feat: path 추가
    
    * :sparkles: Feat: 노출 범위 설정 페이지 구현
    
    * :sparkles: Feat: 동네소식 콘텐츠 제작 페이지 구현
    
    * :sparkles: Feat: 동네소식 콘텐츠 임시저장 페이지 ui 및 로직 일부 구현
    
    * :sparkles: Feat: 동네소식 등록 완료 페이지 구현 및 관련 훅 구현
    
    * Update Form.tsx
    
    * :recycle: Refactor: 코드리뷰 피드백 반영
    
    * :recycle: Refactor: api 컨벤션에 맞게 수정
    
    * :recycle: Refactor: react-query 코드에 타입 추가
    
    * :fire: Remove: 불필요한 파일 삭제
    
    * :speech_balloon: Chore: 불필요한 코드 삭제
    
    * :sparkles: Feat: 임시 저장 등록 기능 구현
    
    * :sparkles: Feat: 임시저장 비활성화
    
    * :sparkles: Feat: 이미지 url을 file 객체로 바꿔주는 유틸함수 구현
    
    * :sparkles: Feat: 임시저장 관련 type 정의
    
    * :speech_balloon: Chore: 언택 매니저님께서 구현하신 formdata에 데이터 넣는 함수 사용을 위해 추가
    
    * :sparkles: Feat: 임시저장 수정 react-query 작성
    
    * :sparkles: Feat: 임시저장 조회 및 삭제 react-query 작성
    
    * :sparkles: Feat: 임시저장 기능 구현
    
    * :sparkles: Feat: 임시저장했던 콘텐츠를 불러와 form에 띄우기 위한 코드 작성 및 수정
    
    * :sparkles: Feat: 최초등록과 임시저장했던 콘텐츠 등록 로직 작성
    
    * :speech_balloon: Chore: 임시저장 관련 hooks을 registerForm hooks으로 이동
    
    * :wrench: Chore: 임시저장 콘텐츠 썸네일 보여지도록 next config.js에 이미지 경로 추가
    
    * :sparkles: Feat: 동영 매니저님 코드 사용하여 무한 스크롤 구현
    
    * :lipstick: Design: lint-css 적용
    
    * :recycle: Refactor: build로 인한 코드 수정
    
    * :recycle: Refactor: 임시저장 콘텐츠 목록 불러오기 위해 코드 수정
    
    * :bug: Fix: 썸네일 이미지 관련 에러 수정
    
    - 다른 콘텐츠 불러왔을 때, 이전 콘텐츠의 썸네일이 뜨는 현상
    - 임시저장 했던 콘텐츠를 수정할 때, 썸네일 파일이 있는 경우에만 formdata 보내도록 수정
    -
    
    * :wrench: Chore: Merge branch 'develop' into feature/CONT_0105
    
    * :recycle: Refactor: Frame 컴포넌트에 className props 추가
    
    * :bug: Fix: 동네소식 등록 페이지 버튼 컴포넌트 잘리는 현상 수정
    
    * :pencil2: Chore: 불필요한 코드 제거

---
+++ convertURLtoFile.ts
@@ -0,0 +1,13 @@
+export const convertURLtoFile = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.blob();
+    const ext = url.split('.').pop();
+    const filename = url.split('/').pop();
+    const metadata = { type: ext === 'jpg' ? `image/jpeg` : `image/${ext}` };
+    return new File([data], filename!, metadata);
+  } catch (error) {
+    console.error('Failed to convert URL to File:', error);
+    throw error;
+  }
+};
--- form.ts
+++ form.ts
@@ -1,10 +1,36 @@
-import { has } from 'lodash-es';
+import { has, isEmpty, negate, pick, some } from 'lodash-es';
 
-import { KeyOf } from '@/types/common';
+import { KeyOf, Many } from '@/types/common';
 
 export const hasKey = <T extends object>(
   obj: T,
   key: string
 ): key is KeyOf<T> => {
   return has(obj, key);
+};
+
+export const hasEmptyValues = <T extends object, K extends KeyOf<T>>(
+  obj: T,
+  ...props: Array<Many<K>>
+): boolean => some(pick(obj, ...props), isEmpty);
+
+export const hasNotEmptyValues = <T extends object, K extends KeyOf<T>>(
+  obj: T,
+  ...props: Array<Many<K>>
+): boolean => some(pick(obj, ...props), negate(isEmpty));
+
+// FormData에 데이터를 추가하는 함수
+export const appendFormData = (formData: FormData, data: any, prefix = '') => {
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const value = data[key];
+      const newKey = prefix ? `${prefix}.${key}` : key;
+
+      if (value instanceof Object && !(value instanceof File)) {
+        appendFormData(formData, value, newKey);
+      } else {
+        formData.append(newKey, value);
+      }
+    }
+  }
 };
