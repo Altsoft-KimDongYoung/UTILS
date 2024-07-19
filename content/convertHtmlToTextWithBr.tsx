5b646d7d44a2d53a524bb93e01d1944b949074d8
an.je@altsoft.kr <155497479+jeongeum1202@users.noreply.github.com>
2024-07-19 17:03:30 +0900

    :sparkles: Feat: 에디터 내용 글자수를 react-quill과 싱크 맞추기 위해 유틸함수 수정

--- content/convertHtmlToTextWithBr.tsx
+++ content/convertHtmlToTextWithBr.tsx
@@ -6,12 +6,21 @@
  *
  * 마바사
  */
-export function convertHtmlToTextWithBr(html: string) {
-  // 1. <br> 태그를 줄바꿈(\n)으로 대체
-  let text = html.replace(/<br\s*\/?>/gi, '\n\n');
 
-  // 2. 나머지 모든 HTML 태그 제거
+export function convertHtmlToTextWithBr(html: string) {
+  // 1. <p><br></p> 태그를 줄바꿈(\n\n)으로 대체
+  let text = html.replace(/<p>\s*<br\s*\/?>\s*<\/p>/gi, '\n');
+
+  // 2. <p> 시작 태그를 제거
+  text = text.replace(/<p>/gi, '');
+
+  // 3. </p> 태그, </ul> 태그, </ol> 태그를 줄바꿈(\n)으로 대체
+  text = text.replace(/<\/p>/gi, '\n');
+  text = text.replace(/<\/ol>/gi, '\n');
+  text = text.replace(/<\/ul>/gi, '\n');
+
+  // 4. 나머지 모든 HTML 태그 제거
   text = text.replace(/<\/?[^>]+(>|$)/g, '');
 
-  return text;
+  return text.trim();
 }
