4b87d44fa8ab3342062a6625034a65e381890dfc
an.je@altsoft.kr <155497479+jeongeum1202@users.noreply.github.com>
2024-07-22 14:09:55 +0900

    [FIX] 소개글, react-quill 에디터(콘텐츠 내용) 글자수 카운팅 이슈 (#272)
    * :bug: Fix: 동네소식 등록 - 에러메시지 이슈 해결
    
    - 제목 글자수 초과 에러메시지 추가
    - 대표이미지 넣은 후에도 파일 첨부 에러메시지 뜨는 이슈 해결
    
    * :bug: Fix: 동네소식 수정 - 에러메시지, 글자수 카운트 이슈 해결
    
    - hookform 컴포넌트로 변경
    - 제목 글자수 카운트하고 에러메시지 띄우도록 수정
    - react-quill 에디터 글자수 초과 시 대응
    
    * :bug: Fix: 동네소식 수정 제목 글자수 초과 및 대표이미지 없을 경우 에러메시지 안뜨는 이슈 해결
    
    * :bug: Fix: 주민소식 메인 등록 - 제목 글자수 초과 시 에러메시지 안뜨는 이슈 해결
    
    * :bug: Fix: 주민소식 사이니지 등록 - 제목 글자수 초과 시 에러메시지 안뜨는 이슈 해결
    
    * :bug: Fix: 주민소식 사이니지 등록 - 소개글 글자수 초과 시 에러표시 안뜨는 이슈 해결
    
    * :bug: Fix: 주민소식 믹스 등록 - 제목, 소개글 글자수 초과 시 에러 메시지 안뜨는 이슈 해결
    
    * :bug: Fix: 광고 메인 등록 - 제목 글자수 초과 시 에러 메시지 안뜨는 이슈 해결
    
    * :bug: Fix: 광고 사이니지 등록 - 제목, 소개글 글자수 초과 시 에러 메시지 안뜨는 이슈 해결
    
    * :bug: Fix: 광고 믹스 등록 - 제목, 소개글 글자수 초과 시 에러 메시지 안뜨는 이슈 해결
    
    - 새롭게 발견한 이슈 → 한글이 아닌 영어일 경우 글자수 초과 시 대응이 먹히지 않음
    
    * :bug: Fix: 광고 메인, 사이니지, 믹스 수정 - 제목, 사이니지 소개글 글자수 초과 시 에러 메시지 안뜨는 이슈 해결
    
    * :pencil2: Chore: 코드 되돌리기
    
    * :sparkles: Feat: 태그제거하고 순수 텍스트만 추출하는 유틸함수 추가
    
    * :sparkles: Feat: 글자수 싱크를 맞추기 위해 유틸함수 적용
    
    * :recycle: Refactor: react-quill 에디터 change 함수 로직 수정 (일부 미완성)
    
    - hookform과의 싱크가 안맞는 것으로 추정
    
    * :bug: Fix: 동,주,광 모든 콘텐츠의 제목, 사이니지 소개글 글자수 초과 시 에러메시지 뜨도록 대응
    
    * :bug: Fix: 콘텐츠 수정 및 추가제작 시 글자수 수정
    
    * :bug: Fix: 콘텐츠 등록 시 에디터 훅에서 관리하는 글자수를 사용하도록 수정
    
    * :bug: Fix: 적용되지 않은 유효성 검사 코드 일부 추가
    
    * :bug: Fix: 콘텐츠 등록 및 수정 시, 텍스트 카운팅에 싱크가 맞지 않아 생기는 이슈 해결
    
    - body는 태그가 포함된 값이기 대문에 2097를 max로 둬야 한다.
    - bodyText가 실제 에디터에 작성하는 글자의 수이고 quill 특성 상 줄바꿈을 포함하기 때문에 2001자를 max로 둬야 한다.
    - 최종 등록 시 유효성 검사는 body 뿐만 아니라 bodyText도 함께 한다. 이유는 bodyText는 글자수 검사를 위함이다.
    
    * :sparkles: Feat: 콘텐츠 수정 및 추가제작 시 contentLength 길이 수정
    
    * :sparkles: Feat: 소개글 개행문자로 인한 글자수 싱크 안맞는 문제 유틸함수 사용하여 해결
    
    * :pencil2: Chore: 주석 삭제
    
    * :sparkles: Feat: 에디터 내용 글자수를 react-quill과 싱크 맞추기 위해 유틸함수 수정
    
    * :bug: Fix: 제목 및 소개글 글자수 초과 시 대응을 위한 코드 추가
    
    * :bug: Fix: 해시태그 값 가져오도록 코드 추가(수정 중)
    
    * :lipstick: Design: 글자수 위치 조정 (한번 더 수정 필요)

---
+++ content/convertHtmlToTextWithBr.tsx
@@ -0,0 +1,26 @@
+/** @Util html 태그가 붙은 문자열을 줄바꿈 포함하여 순수 텍스트로 변경하는 유틸함수
+ * ex) const htmlString = "<p>가나다라</p><p><br></p><p><strong>마바사</p>";
+ * const result = convertHtmlToTextWithBr(htmlString);
+ *
+ * 가나다라
+ *
+ * 마바사
+ */
+
+export function convertHtmlToTextWithBr(html: string) {
+  // 1. <p><br></p> 태그를 줄바꿈(\n\n)으로 대체
+  let text = html.replace(/<p>\s*<br\s*\/?>\s*<\/p>/gi, '\n');
+
+  // 2. <p> 시작 태그를 제거
+  text = text.replace(/<p>/gi, '');
+
+  // 3. </p> 태그, </ul> 태그, </ol> 태그를 줄바꿈(\n)으로 대체
+  text = text.replace(/<\/p>/gi, '\n');
+  text = text.replace(/<\/ol>/gi, '\n');
+  text = text.replace(/<\/ul>/gi, '\n');
+
+  // 4. 나머지 모든 HTML 태그 제거
+  text = text.replace(/<\/?[^>]+(>|$)/g, '');
+
+  return text.trim();
+}
