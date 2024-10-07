a99ae71bf4f6c123986a0035ac39e2b3a8c86072
kim.dy@altsoft.kr <155496890+Altsoft-KimDongYoung@users.noreply.github.com>
2024-10-07 09:19:48 +0900

    [COMMON] Badge 컴포넌트  (#455)
    * :recycle: Refactor: 콘텐츠 프로필 ui 변경
    
    - 뱃지 추가
    - 로컬박스 동 삭제
    - 메인피드와 상세피드 분기처리
    - regDt 추가
    
    * :recycle: Refactor: 시간계산 유틸함수 하나로 통일하고 모든 코드에 적용, regDt props 추가
    
    * :lipstick: Design: 댓글 프로필 컴포넌트 UI 변경
    
    * :recycle: Refactor: 시간 유틸 함수에서 같은 년도일 경우 년도 생략하는 정책 반영
    
    * :recycle: Refactor: 코드리뷰 피드백 반영
    
    * :white_check_mark: Test
    
    * ♻️ Refactor: interface로 타입선언 변경
    
    * ♻️ Refactor: 마이그레이션을 위한 기존 뱃지 폴더 네이밍 변경
    
    * ✨ Feat: 뱃지 컴포넌트
    
    * ♻️ Refactor: 아이콘 뱃지, 텍스트 뱃지 분리
    
    * ♻️ Refactor: 바뀐 컴포넌트 도메인 적용
    
    * ♻️ Refactor: 바뀐 컴포넌트 도메인 적용
    
    * ♻️ Refactor: IconBadge css props 추가
    
    * ✨ Feat: position props 추가
    
    * ✨ Feat: position props 적용
    
    * ♻️ Refactor: pinIconStyles 제거
    
    * 💄 Design: lint-css 적용
    
    * ♻️ Refactor: 피드백 반영
    
    ---------
    
    Co-authored-by: an.je@altsoft.kr <155497479+jeongeum1202@users.noreply.github.com>

--- time.ts
+++ time.ts
@@ -2,30 +2,6 @@ import dayjs from 'dayjs';
 
 import assert from './assert';
 
-/* 시간 계산 함수 */
-export const timeFuc = (commentTime: string) => {
-  const today = new Date();
-  const timeValue = new Date(commentTime);
-
-  const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
-  if (betweenTime < 1) return '방금 전';
-  if (betweenTime < 60) {
-    return `${betweenTime}분 전`;
-  }
-
-  const betweenTimeHour = Math.floor(betweenTime / 60);
-  if (betweenTimeHour < 24) {
-    return `${betweenTimeHour}시간 전`;
-  }
-
-  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
-  if (betweenTimeDay < 365) {
-    return `${betweenTimeDay}일 전`;
-  }
-
-  return `${Math.floor(betweenTimeDay / 365)}년 전`;
-};
-
 /**
  * @Utils 문자열 형태의 날짜/시간을 받고 현재 시간/날짜와 계산하여
  * 형식에 맞는 문자열로 반환한다.
@@ -49,6 +25,10 @@ export const convertNewTextToDate = (date: string) => {
 
   if (diffInHours < 24) {
     return `${diffInHours}시간전`;
+  }
+
+  if (compareToDate.year() === currentDate.year()) {
+    return compareToDate.format('MM.DD');
   }
 
   return compareToDate.format('YYYY.MM.DD');
