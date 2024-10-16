753d63bc5ba7e493a3187324cd2ba20a33de57db
hong.ut@altsoft.kr <155497193+HongUnTaek@users.noreply.github.com>
2024-10-16 17:21:57 +0900

    [COMMON] Navigation 컴포넌트 리팩토링 (#467)
    * 🍱 Chore: Navigation 컴포넌트에서 사용되는 SVG 추가
    
    * ✅ Test: Shorts Root 페이지 생성
    
    * ✨ Feat: isPathMatch -> 현재 경로와 매칭되는지 여부를 반환하는 유틸함수 구현
    
    * ✨ Feat: Shorts Root 경로 추가
    
    * 🐛 Fix: 닫는태그 잘못된부분 수정
    
    * ✨ Feat: 공통 훅 구현
    
    * ✨ Feat: Navigation 로직 분리하여 구현
    
    * ✏️ Chore: Type 컨벤션 적용
    
    * ✨ Feat: navigationVariant 추가
    
    * 💄 Design: 사용안되는 스타일 제거 및 variant 분기
    
    * ♻️ Refactor: UI / 로직 분리
    
    * ♻️ Refactor: 기존 FCM 푸시 메시지 관련 로직 제거 및 디자인 수정
    
    * 🐛 Fix: navigationVariant defaultValue 추가
    
    * ✏️ Chore: Clean Code
    
    * 💄 Design: lint-css 적용
    
    * ✨ Feat: 스토리북 구현
    
    * 🔥 Remove: 테스트용 Shorts 앱라우팅 삭제
    
    * 🍱 Chore: ShortForm GNB에서 사용할 gray40아이콘 추가 및 사용되지 않는 white아이콘 제거
    
    * ✨ Feat: 아이콘 변경으로 인한 컴포넌트 수정
    
    * ✨ Feat: 디자인 변경으로 인한 컴포넌트 수정
    
    * 🐛 Fix: Frame 컨플릭 버그 수정

--- path.ts
+++ path.ts
@@ -1,7 +1,19 @@
+import { usePathname } from 'next/navigation';
+
 /** @Utils pathname 중 원하는 Depth를 추출한다. */
 export const getDepthPath = (path: string, depth: number) => {
   const segments = path.split('/').filter((path) => path);
   const depthSegment = segments[depth - 1] || path;
 
   return depthSegment;
+};
+
+/**
+ * @Utils 현재 경로와 타겟 경로가 일치하는지 확인하는 함수
+ * @returns 일치하면 true, 아니면 false
+ */
+export const isPathMatch = (targetPath: string) => {
+  const pathname = usePathname();
+
+  return pathname === targetPath;
 };
