d5b841e6d17d2692a444aaf5033b537f0773f460
an.je@altsoft.kr <155497479+jeongeum1202@users.noreply.github.com>
2024-10-16 10:19:40 +0900

    [COMMON] Input File 컴포넌트 (#462)
    * :pencil2: Chore: 조사에 '은,는' 케이스 추가
    
    * :pencil2: Chore: 콘텐츠 대표이미지 사이즈 상수 수정
    
    * :bento: Chore: 동영상 업로드 관련 아이콘 추가
    
    * :sparkles: Feat: 비디오 태그 구현
    
    * :recycle: Refactor: 대표이미지 및 영상 업로드 컴포넌트 리팩토링
    
    - 뱃지 텍스트 변경
    - contentType 변경
    - 캡션 추가
    - 콘텐츠 비율 (주민, 광고에서 가로세로) 추가
    - onChange 핸들러 내 로직 분리
    - 동영상 업로드 가능하도록 변경
    
    * :recycle: Refactor: contentType 변경으로 인한 코드 수정
    
    * :white_check_mark: Test: 동네소식 제작을 통해 동영상 업로드 테스트
    
    * :sparkles: Feat: 동영상일 경우 재생 시간 검사 로직 추가
    
    * :lipstick: Design: 콘텐츠 비율에 따른 css 버그 수정
    
    * :white_check_mark: Test: 주민소식 콘텐츠 제작에 적용 테스트
    
    * :sparkles: Feat: content upload hookform 스토리북 작성
    
    * :bug: Fix: 정책 반영이 되지 않는 코드 수정
    
    * :recycle: Refactor: 타입 추가, 함수 네이밍 변경, 불필요한 코드 삭제
    
    * :pencil2: Chore: 스토리북 코드 수정
    
    * :pencil2: Chore: 불필요한 코드 삭제
    
    * :sparkles: Feat: ContentUploadHookForm 컴포넌트 스토리북 작성
    
    * :pencil2: Chore: 테스트용 코드 주석처리한 것 삭제
    
    * :lipstick: Design: lint-css로 인한 코드 수정
    
    * :recycle: Refactor: 조사 유틸함수 리팩토링
    
    * :recycle: Refactor: 리팩토링한 조사 유틸함수 적용
    
    * :pencil2: Chore: 조사 유틸함수에 사용되는 boolean 타입의 params 네이밍 변경
    
    * :recycle: Refactor: 비디오 태그에 작성된 비디오 관련 로직 훅으로 분리
    
    * :lipstick: Design: z-index 토큰 적용
    
    * :sparkles: Feat: 비디오 컴포넌트 스토리북 작성
    
    * :pencil2: Chore: 코드 정리
    
    * :pencil2: Chore: 코드리뷰 피드백 반영 - 변수 선언으로 중복요소 줄이기
    
    * :recycle: Refactor: 코드리뷰 피드백 반영 - boolean 값 컨벤션에 맞게 수정
    
    * :bug: Fix: 코드리뷰 피드백 반영 - mov 확장자 MIME 타입 변경
    
    * :recycle: Refactor: 코드리뷰 피드백 반영 - 작은 너비의 영상일 경우, 좌측으로 영상이 쏠려보이는 이슈 수정
    
    * :recycle: Refactor: 코드리뷰 피드백 반영 - padStart 메서드 사용하여 가독성 높이기
    
    * :recycle: Refactor: 코드리뷰 피드백 반영 - addJosa, getJosa 함수 분리
    
    * :recycle: Refactor: getJosa 함수 적용
    
    * :recycle: Refactor: 영상 길이 30초 이내인 점 정확하게 반영
    
    - 30.n초도 업로드 가능하도록 변경
    
    * :bug: Fix: 주민소식에 필요한 contentRatio가 없어도 될 경우(동네이야기 등) 기본값 설정하여 영상 보이도록 수정
    
    * :pencil2: Chore: mb 확장자 표기를 대문자로 통일
    
    * :recycle: Refactor: 영상 길이 제한 수정

--- josa.ts
+++ josa.ts
@@ -1,4 +1,4 @@
-export type JosaOption = '이' | '가' | '이/가' | '을' | '를' | '을/를' | '으로';
+export type JosaOption = '이' | '가' | '이/가' | '을' | '를' | '을/를' | '으로' | '은' | '는';
 
 const checkKorean = (word: string) => {
   const lastChar = word.charCodeAt(word.length - 1);
@@ -19,14 +19,30 @@ const formats = (word: string, josa: JosaOption) => {
       return checkKorean(word) ? '을' : '를';
     case '으로':
       return checkKorean(word) ? '으로' : '로';
+    case '은':
+    case '는':
+      return checkKorean(word) ? '은' : '는';
     default:
       throw 'Invalid format!';
   }
 };
 
-export const addJosa = (word: string, josa: JosaOption) => {
-  if (typeof josa === 'undefined') throw 'invalid format!';
+/**
+ * @Utils 단어와 조사를 붙여서 반환하거나, 조사만 반환하는 유틸함수
+ * word - 조사와 함께할 단어
+ * josa - 사용할 조사 옵션
+ * isWithWord - true일 경우 단어+조사를 반환, false일 경우 조사만 반환
+ * return -단어+조사 또는 조사만 반환
+ */
+
+export const getJosa = (word: string, josa: JosaOption) => {
   const formattedJosa = formats(word, josa);
 
   return formattedJosa;
+};
+
+export const addJosa = (word: string, josa: JosaOption) => {
+  const josaValue = getJosa(word, josa);
+
+  return `${word}${josaValue}`;
 };
